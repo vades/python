51c109110b4348722e1c1ca0f8ea19001c20a65b
Martin Vach <vades.cz@gmail.com>
2023-06-07 09:45:20 +0100

    Adding functions

---
+++ basics/functions.py
@@ -0,0 +1,17 @@
+# How to define and call a function
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+greet('Jane')
+
+# Using default parameter values
+
+
+def square(x):
+    return x * x
+
+
+print(square(5))
+
+# Using default parameter values
