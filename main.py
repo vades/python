a5c684d5cb1775ef121b8031a117b6d060241f19
Martin Vach <vades.cz@gmail.com>
2023-06-14 09:36:12 +0100

    Implementing environments

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from settings.config import cfg
-from dotenv import load_dotenv
 import os
+from dotenv import dotenv_values
 
-load_dotenv()
-print(os.getenv('MYENV'))
+config = {
+    # load shared development variables
+    **dotenv_values("environments/.env.shared"),
+    **dotenv_values("environments/.env"),  # load sensitive variables
+    **os.environ,  # override loaded values with environment variables
+}
 
 
-print(cfg.app)
+print(config['APP_NAME'])
