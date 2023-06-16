374c97e76cf09f0d38ed25c1eaf667da2743305e
Martin Vach <vades.cz@gmail.com>
2023-06-16 16:24:11 +0100

    Downloading video and generating mp3

--- apps/ytb/ytbhandler.py
+++ apps/ytb/ytbhandler.py
@@ -4,9 +4,10 @@ from slugify import slugify
 
 
 class YtbHandler:
-    def __init__(self, video_id):
+    def __init__(self, config, video_id):
+        self.cfg = config
         self.video_id = video_id
-        self.video_url = 'https://www.youtube.com/watch?v=' + video_id
+        self.video_url = config['YTB_VIDEO_URL'] + video_id
 
     def download_video(self):
         """
@@ -16,22 +17,24 @@ class YtbHandler:
             video (str): Path to the file.
         """
         ytb = YouTube(self.video_url)
-        # video_slug = slugify(ytb.title)
-        filename = self.video_id + '.mp4'
+        video_slug = slugify(ytb.title, max_length=32)
+        filename = f'{video_slug}-{self.video_id}.mp4'
         video_stream = ytb.streams.get_highest_resolution()
-        video_stream.download(output_path="./_download", filename=filename)
+        video_stream.download(
+            output_path=self.cfg['YTB_DOWNLOAD_DIR'], filename=filename)
 
         return filename
 
-    def video_to_audio(self, video):
+    def video_to_audio(self, video_file):
         """
         Convert downloaded video to audio in MP3 format.
 
         Args:
             video (str): Path to the file.
         """
-        video_clip = mp.VideoFileClip(video)
+        downloaded_video = self.cfg['YTB_DOWNLOAD_DIR'] + '/' + video_file
+        video_clip = mp.VideoFileClip(downloaded_video)
         audio_clip = video_clip.audio
-        audio_clip.write_audiofile(f"{video[:-4]}.mp3")
+        audio_clip.write_audiofile(
+            f"{self.cfg['MP3_OUTPUT_DIR']}/{video_file[:-4]}.mp3")
         audio_clip.close()
-        video_clip.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import dotenv_values
-
+from apps.ytb.ytbhandler import YtbHandler
 config = {
     # load shared development variables
     **dotenv_values("environments/.env.shared"),
@@ -8,5 +8,7 @@ config = {
     **os.environ,  # override loaded values with environment variables
 }
 
-
-print(config['APP_NAME'])
+ytb = YtbHandler(config, 'Gtp6WkuoClM')
+downloaded_video = ytb.download_video()
+ytb.video_to_audio(downloaded_video)
+print(config['APP_DOWNLOAD_DIR'])
