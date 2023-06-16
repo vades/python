03e1cf24903b10512f0509ec7b162d37212ab735
Martin Vach <vades.cz@gmail.com>
2023-06-16 08:46:42 +0100

    Updating functions

--- basics/functions.py
+++ basics/functions.py
@@ -62,3 +62,47 @@ max_number = max(numbers, key=lambda x: x)
 print(max_number)  # Output: 10
 
 # Passing functions as arguments
+
+
+def apply(func, x):
+    return func(x)
+
+
+def square2(x):
+    return x * x
+
+
+result = apply(square2, 5)
+print(result)   # output: 25
+
+# Using closures
+
+
+def add(x):
+    def inner(y):
+        return x + y
+    return inner
+
+
+add5 = add(5)
+result = add5(3)
+print(result)   # output: 8
+
+# Using decorators
+
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Before the function is called.")
+        result = func(*args, **kwargs)
+        print("After the function is called.")
+        return result
+    return wrapper
+
+
+@my_decorator
+def say_hello(name):
+    print(f"Hello, {name}!")
+
+
+say_hello("John")
