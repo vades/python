3e7b464b04747fd574a7daf28655ecd839e87b12
Martin Vach <vades.cz@gmail.com>
2023-06-19 10:59:12 +0100

    Config from NestedNamespace

--- apps/ytb/ytbhandler.py
+++ apps/ytb/ytbhandler.py
@@ -1,13 +1,15 @@
 from pytube import YouTube
+from pytube.exceptions import PytubeError, RegexMatchError, VideoUnavailable, MembersOnly, RecordingUnavailable, LiveStreamError
 import moviepy.editor as mp
+# from moviepy.editor import MoviePyError
 from slugify import slugify
 
 
 class YtbHandler:
     def __init__(self, config, video_id):
-        self.cfg = config
+        self.config = config
         self.video_id = video_id
-        self.video_url = config['YTB_VIDEO_URL'] + video_id
+        self.video_url = config.ytb.video_url + video_id
 
     def download_video(self):
         """
@@ -16,14 +18,18 @@ class YtbHandler:
         Args:
             video (str): Path to the file.
         """
-        ytb = YouTube(self.video_url)
-        video_slug = slugify(ytb.title, max_length=32)
-        filename = f'{video_slug}-{self.video_id}.mp4'
-        video_stream = ytb.streams.get_highest_resolution()
-        video_stream.download(
-            output_path=self.cfg['YTB_DOWNLOAD_DIR'], filename=filename)
 
-        return filename
+        try:
+            ytb = YouTube(self.video_url)
+            video_slug = slugify(ytb.title, max_length=32)
+            filename = f'{video_slug}-{self.video_id}.mp4'
+            video_stream = ytb.streams.get_highest_resolution()
+            video_stream.download(
+                output_path=self.config.ytb.download_dir, filename=filename)
+            return filename
+        except PytubeError as e:
+            # Handle general pytube exceptions
+            print("A Pytube error occurred:", str(e))
 
     def video_to_audio(self, video_file):
         """
@@ -32,9 +38,13 @@ class YtbHandler:
         Args:
             video (str): Path to the file.
         """
-        downloaded_video = self.cfg['YTB_DOWNLOAD_DIR'] + '/' + video_file
-        video_clip = mp.VideoFileClip(downloaded_video)
-        audio_clip = video_clip.audio
-        audio_clip.write_audiofile(
-            f"{self.cfg['MP3_OUTPUT_DIR']}/{video_file[:-4]}.mp3")
-        audio_clip.close()
+        try:
+            downloaded_video = f'{self.config.ytb.download_dir}/{video_file}'
+            video_clip = mp.VideoFileClip(downloaded_video)
+            audio_clip = video_clip.audio
+            audio_output = f'{self.config.mp3_output_dir}/{video_file[:-4]}.mp3'
+            audio_clip.write_audiofile(audio_output)
+            audio_clip.close()
+        except Exception as e:
+            # Exception handling code
+            print("MoviePyError occurred:", str(e))
--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import os
-from dotenv import dotenv_values
+
+# from libs.nestednamespace import NestedNamespace
+from environments.config import config
 from apps.ytb.ytbhandler import YtbHandler
-config = {
+""" config = {
     # load shared development variables
     **dotenv_values("environments/.env.shared"),
     **dotenv_values("environments/.env"),  # load sensitive variables
     **os.environ,  # override loaded values with environment variables
+} """
+
+
+def print_error_message(message):
+    RED = "\033[91m"
+    RESET = "\033[0m"
+    print(f"{RED}Error: {message}{RESET}")
+
+
+# Example usage
+error_message = "Something went wrong!"
+print_error_message(error_message)
+
+print('\n*********** Initializing video YouTube: ***********')
+ytb = YtbHandler(config, 'Gtp6WkuoClM')
+print('\n*********** Downloading video: ***********')
+# downloaded_video = ytb.download_video()
+downloaded_video = 'python-for-beginners-3-chat-gpt-Gtp6WkuoClM.mp4'
+ytb.video_to_audio(downloaded_video)
+""" config_dict = {
+    'parent': {
+        'child': {
+            'grandchild': 'value'
+        }
+    },
+    'normal_key': 'normal value',
 }
 
-ytb = YtbHandler(config, 'Gtp6WkuoClM')
-downloaded_video = ytb.download_video()
-ytb.video_to_audio(downloaded_video)
-print(config['APP_DOWNLOAD_DIR'])
+config = NestedNamespace(config_dict) """
+
+""" print(config)  # value
+print(config.secret_key)  # normal value """
